925b650de31bdefad0a707ec9af81304f67159ff
Alex Claude <abauruel@gmail.com>
2019-01-20 10:47:40 -0200

    finalizado funcionalidades de issues redirecinando para navegador

--- src/pages/Repositories/RepositoriesItems/index.js
+++ src/pages/Repositories/RepositoriesItems/index.js
@@ -8,21 +8,21 @@ import PropTypes from 'prop-types';
 
 import styles from './styles';
 
-const RepositoriesItems = ({ repos, repoIssue }) => (
-  <View style={styles.container}>
-    <Image style={styles.img} source={{ uri: repos.organization.avatar_url }} />
-    <View style={styles.names}>
-      <Text style={styles.name}>{repos.name}</Text>
-      <Text style={styles.login}>
-        {repos.organization.login ? repos.organization.login : repos.full_name}
-      </Text>
+const RepositoriesItems = ({ repos, repoIssue }) => {
+  console.tron.log('repos', repos);
+  return (
+    <View style={styles.container}>
+      <Image style={styles.img} source={{ uri: repos.avatar_url }} />
+      <View style={styles.names}>
+        <Text style={styles.name}>{repos.name}</Text>
+        <Text style={styles.login}>{repos.login ? repos.login : repos.full_name}</Text>
+      </View>
+      <TouchableOpacity style={styles.button} onPress={() => repoIssue(repos.full_name)}>
+        <Icon name="angle-right" size={16} style={styles.seta} />
+      </TouchableOpacity>
     </View>
-    <TouchableOpacity style={styles.button} onPress={() => repoIssue(repos.full_name)}>
-      <Icon name="angle-right" size={16} style={styles.seta} />
-    </TouchableOpacity>
-  </View>
-);
-
+  );
+};
 RepositoriesItems.propTypes = {
   repos: PropTypes.shape({
     name: PropTypes.string,
--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -20,11 +20,9 @@ export default class Repositories extends Component {
     headerTitleContainerStyle:{
       justifyContent:'center',
       fontWeight: 'bold',
-      
-       
     }
-    
   }
+
   state = {
     repositories: [],
     refreshing: false,
@@ -32,11 +30,14 @@ export default class Repositories extends Component {
     loading: false,
   };
   async componentWillMount() {
+    
     const repos = await AsyncStorage.getItem('@Desafio3:repositories');
+    if(repos){
     this.setState({
       repositories: JSON.parse(repos),
       refreshing: false,
-    });
+    })}
+
   }
 
 
@@ -48,38 +49,48 @@ export default class Repositories extends Component {
       repositories: JSON.parse(repos),
       refreshing: false,
     });
-    console.tron.log(repositories)
+
   }
 
   insertRepos = async () => {
     const { repo, repositories } = this.state;
     const { data } = await api.get(`/repos/${repo}`);
+    const varRepositories = {
+      id: data.id,
+      avatar_url: data.organization.avatar_url,
+      login: data.organization.login,
+      name: data.name,
+      full_name: data.full_name,
+    }
     this.setState({
-      repositories: [...repositories, data],
+      repositories: [...repositories, varRepositories]
     });
     AsyncStorage.clear();
-    await AsyncStorage.setItem('@Desafio3:repositories', JSON.stringify(repositories));
+    const stateRepositories = JSON.stringify(repositories);
+    AsyncStorage.setItem('@Desafio3:repositories', stateRepositories);
+
   };
 
   changeRepo = text => this.setState({ repo: text });
 
-  renderListItem = ({ item }) => (
+  renderListItem = ({item}) => (
     <RepositoriesItem repos={item} repoIssue={this.repoIssue}/>
   )
 
   repoIssue = name => {
     const { navigation } = this.props
-
     navigation.navigate('Issues',{ repositorio: `${name}`})
-
   }
   renderList = () => {
     const { repositories, refreshing } = this.state;
     return (
-      <FlatList data={repositories}  keyExtractor={item => String(item.id)}
+      <FlatList 
+        data={repositories}  
+        keyExtractor={item => String(item.id)}
         renderItem={this.renderListItem}
         onRefresh={this.loadingRepository}
         refreshing={refreshing} />
+    
     );
   };
 
@@ -87,7 +98,7 @@ export default class Repositories extends Component {
     const { repo, loading } = this.state;
     return (
       <View style={styles.container}>
-        
+
         <Form insertRepos={this.insertRepos} name={repo} changeRepo={this.changeRepo} />
         {loading ? <ActivityIndicator style={styles.loading} /> : this.renderList()}
       </View>
